2564e1dd01c80a54c3329d62751b3c3a1f63512b
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-07-20 22:36:45 +0430

    C#_Designer_PDF_Viewer

---
+++ Designer_PDF_Viewer.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        List<KeyValuePair<char, int>> _list = new List<KeyValuePair<char, int>>();
+        int counter = 0;
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            _list.Add(new KeyValuePair<char, int>(c, h[counter]));
+            counter++;
+        }
+
+        int maxHeight = 0;
+        foreach (var item in word)
+        {
+            var height = _list.Where(q => q.Key == item).FirstOrDefault().Value;
+
+            if (height > maxHeight)
+                maxHeight = height;
+        }
+        return maxHeight * word.Length;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
