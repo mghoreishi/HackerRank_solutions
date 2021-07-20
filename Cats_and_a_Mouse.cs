9459774be7c9f2d0a7c2a87ecc168a6fb1f32ba3
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-07-20 15:52:17 +0430

    C#_Cats_and_a_Mouse

---
+++ Cats_and_a_Mouse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    class Solution
+    {
+
+        // Complete the catAndMouse function below.
+        static string catAndMouse(int x, int y, int z)
+        {
+            if (Math.Abs(x - z) > Math.Abs(y - z))
+                return "Cat B";
+            else if (Math.Abs(x - z) < Math.Abs(y - z))
+                return "Cat A";
+            else
+                return "Mouse C";
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string[] xyz = Console.ReadLine().Split(' ');
+
+                int x = Convert.ToInt32(xyz[0]);
+
+                int y = Convert.ToInt32(xyz[1]);
+
+                int z = Convert.ToInt32(xyz[2]);
+
+                string result = catAndMouse(x, y, z);
+
+                Console.WriteLine(result);
+            }
+
+           
+        }
+    }
+}
