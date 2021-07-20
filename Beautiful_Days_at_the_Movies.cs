15d0e107af413578bad131079287427bc9e7c5fa
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-07-20 23:33:51 +0430

    C#_Beautiful_Days_at_the_Movies

---
+++ Beautiful_Days_at_the_Movies.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int beautifulDays = 0;
+
+        for (int a = i; a <= j; a++)
+        {
+            int r = a.ToString().Reverse().Aggregate(0, (b, x) => 10 * b + x - '0');
+            if (Math.Abs(r - a) % k == 0)
+                beautifulDays++;
+        }
+
+        return beautifulDays;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int i = Convert.ToInt32(firstMultipleInput[0]);
+
+        int j = Convert.ToInt32(firstMultipleInput[1]);
+
+        int k = Convert.ToInt32(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
