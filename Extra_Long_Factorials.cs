6cb6f6b539abd117d62c743970129e755cdc1498
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-07-26 09:36:05 +0430

    C#_Extra_Long_Factorials

---
+++ Extra_Long_Factorials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Extra_Long_Factorials
+    {
+        public static void extraLongFactorials(int n)
+        {
+            System.Numerics.BigInteger res = n;
+            n--;
+            while (n > 1)
+            {
+                res *= n;
+                n--;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
