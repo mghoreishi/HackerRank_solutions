14469bca7ecb2a6d4a81574a59095d153a09d25b
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-08-12 09:42:17 +0430

    C#_Repeated_String

---
+++ Repeated_String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Repeated_String
+    {
+        public static long repeatedString(string s, long n)
+        {
+            long m = n / s.Length;
+            int r = Convert.ToInt32(n % s.Length);
+
+            return s.Count(f => (f == 'a')) * m + s.Substring(0, r).Count(f => (f == 'a'));
+
+        }
+    }
+}
