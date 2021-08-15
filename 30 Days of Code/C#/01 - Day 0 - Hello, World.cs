3d3d2201a02d4f12e7326b4923fc640f3d50da52
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-08-15 16:58:52 +0430

    01 - Day 0 - Hello, World

---
+++ 30 Days of Code/C#/01 - Day 0 - Hello, World.cs
@@ -0,0 +1,12 @@
+// Day 0: Hello, World.
+// Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+
+// Declare a variable named 'inputString' to hold our input.
+String inputString; 
+        
+// Read a full line of input from stdin (cin) and save it to our variable, input_string.
+inputString = Console.ReadLine(); 
+        
+// Print a string literal saying "Hello, World." to stdout using cout.
+Console.WriteLine("Hello, World.");
+Console.WriteLine(inputString);
