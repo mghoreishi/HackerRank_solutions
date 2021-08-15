6f683a00cbb087f0d0871dce163360b4df1d247c
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-08-15 19:08:25 +0430

    26 - Day 25 - Running Time and Complexity.cs

---
+++ 30 Days of Code/C#/26 - Day 25 - Running Time and Complexity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i, j, inputNumber;
+        string output;
+
+        for (i = 1; i <= n; i++) {
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (j = 2; j <= inputNumber / j; j++) {
+                if (inputNumber % j == 0) {
+                    inputNumber = 1;
+                }
+            }
+            output = inputNumber == 1 ? "Not prime": "Prime";
+            Console.WriteLine(output);
+        }
+    }
+}
