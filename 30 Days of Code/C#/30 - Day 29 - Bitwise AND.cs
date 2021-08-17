caebae6c15158bdaaf70098cfa4194594e05fb36
Maryam Ghoreishi <49720023+mghoreishi@users.noreply.github.com>
2021-08-17 09:18:40 +0430

    30 - Day 29 - Bitwise AND.cs

---
+++ 30 Days of Code/C#/30 - Day 29 - Bitwise AND.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K)
+    {
+       if (((K - 1) | K) > N && K % 2 == 0) {
+               return K - 2;
+            }
+            else {
+                return K - 1;
+            }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int count = Convert.ToInt32(firstMultipleInput[0]);
+
+            int lim = Convert.ToInt32(firstMultipleInput[1]);
+
+            int res = Result.bitwiseAnd(count, lim);
+
+            textWriter.WriteLine(res);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
